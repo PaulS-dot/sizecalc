1e1ba1bb358bef0ce17ac53995a5f495de54bdb4
dwstudios <theChawke@gmail.com>
2021-07-25 20:13:30 +0200

    FormHeader to styled

--- src/components/form/FormHeader/FormHeader.js
+++ src/components/form/FormHeader/FormHeader.js
@@ -1,15 +1,15 @@
-import { Box, Heading, Flex, Center, Text } from '@chakra-ui/layout'
 import React from 'react'
-import { ProgressWithSteps, ProgressStep } from './ProgressSteps'
+import {
+  ProgressWithSteps,
+  ProgressStep,
+} from '../ProgressWithSteps/ProgressSteps'
 import { FormHeaderContainer } from './FormHeader.styled'
+import { Heading } from './FormHeader.styled'
 
-// <VStack w="100vw" mb="10">
 export const FormHeader = props => {
   return (
     <FormHeaderContainer>
-      <Heading fontSize="md" mt="8" color="purple.700">
-        Przeliczanie rozmiarów
-      </Heading>
+      <Heading>Przeliczanie rozmiarów</Heading>
       <ProgressWithSteps value={props.step}>
         <ProgressStep>płeć</ProgressStep>
         <ProgressStep>kategoria</ProgressStep>
--- src/components/form/FormHeader/FormHeader.styled.js
+++ src/components/form/FormHeader/FormHeader.styled.js
@@ -1,6 +1,15 @@
 import styled from 'styled-components'
 import { VStack } from 'src/styled/layout'
+import { H1 } from 'src/styled/Headers.styled'
 
-export const FormHeaderContainer = styled(VStack)`
+const FormHeaderContainer = styled(VStack)`
   width: 100vw;
+  margin-bottom: 3rem;
 `
+
+const Heading = styled(H1)`
+  margin: 1rem 0;
+  ${props => props.theme.colors[700]};
+`
+
+export { FormHeaderContainer, Heading }
